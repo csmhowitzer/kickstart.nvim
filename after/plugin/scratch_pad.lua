f2eb4ab389bf7d5825647c6b96f608cdd69d2c2c
csmhowitzer <csmhowitzer@gmail.com>
2025-03-19 13:48:10 -0400

    working on refactoring the scratch pad changes

--- after/plugin/scratch_pad.lua
+++ after/plugin/scratch_pad.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: missing-fields
+
 -- INFO: adjustments made to folke-snacks.nvim plugin
 -- Changes:
 --    - Border Style
@@ -10,6 +12,9 @@
 --      - When you open any file that already has had a Scratch Pad this will open up the old one.
 --      - You will never have a "new" one each time you open one up if one exists
 --
+-- TODO: Properly open a scratch pad when a list of files exists
+--  - update how we open, look at notes
+--  - add a markdown only scratch type
 
 local Snacks = require 'snacks'
 
@@ -33,6 +38,8 @@ local buffer_vals = function(bufnr)
 
   local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ':h')
   local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ':t')
+  local ft = vim.filetype.match { filename = name }
+  local icon = Snacks.util.icon(ft, 'filetype')
 
   path = path and vim.fn.fnamemodify(path, ':p:~')
 
@@ -42,6 +49,9 @@ local buffer_vals = function(bufnr)
     path = path,
     name = name,
     fmtName = fmt_buf_name(name),
+    ft = ft,
+    icon = icon,
+    file = nil,
   }
 
   return pad
@@ -53,45 +63,7 @@ local get_scratch_filename = function(winName)
   return filtered
 end
 
-local open_scratch = function(buffer, vals)
-  Snacks.scratch.open {
-    icon = buffer.icon,
-    file = buffer.file,
-    name = buffer.name,
-    ft = buffer.ft,
-    win = winOpts(vals.width, vals.height),
-  }
-  vim.api.nvim_set_hl(0, 'SnacksInputBorder', { fg = '#F7DC6F' })
-end
-
-local reload_scratch_pad = function(buffers, vals)
-  assert(vals.path, 'no path provided')
-  assert(vals.name, 'no filename provided')
-
-  local scratchPad = {}
-
-  for _, item in ipairs(buffers) do
-    local wd = item.cwd and vim.fn.fnamemodify(item.cwd, ':p:~') or ''
-    local fileName = get_scratch_filename(item.name)
-
-    if fileName == vals.name and wd == vals.path then
-      item.icon = item.icon or Snacks.util.icon(item.ft, 'filetype')
-      item.branch = item.branch and ('branch:%s'):format(item.branch) or ''
-      scratchPad.name = item.name
-      scratchPad.icon = item.icon
-      scratchPad.file = item.file
-      scratchPad.ft = item.ft
-      break
-    end
-  end
-
-  if scratchPad ~= {} and scratchPad.name ~= '' then
-    open_scratch(scratchPad, vals)
-  end
-
-  return scratchPad
-end
-
+-- may want to try using scratch.open
 local open_new_scratch_pad = function(vals)
   Snacks.scratch {
     name = vals.fmtName,
@@ -106,15 +78,67 @@ local open_new_scratch_pad = function(vals)
   vim.api.nvim_set_hl(0, 'SnacksInputBorder', { fg = '#F7DC6F' })
 end
 
+local open_scratch = function(padVals)
+  Snacks.scratch.open {
+    icon = padVals.icon,
+    file = padVals.file,
+    name = padVals.fmtName,
+    ft = padVals.ft,
+    win = winOpts(padVals.width, padVals.height),
+  }
+  vim.api.nvim_set_hl(0, 'SnacksInputBorder', { fg = '#F7DC6F' })
+end
+
+local reload_scratch_pad = function(buffers, vals)
+  assert(vals.path, 'no path provided')
+  assert(vals.name, 'no filename provided')
+
+  local scratchPad = {}
+
+  for _, item in ipairs(buffers) do
+    local wd = item.cwd and vim.fn.fnamemodify(item.cwd, ':p:~') or '' -- ternary
+    local fileName = get_scratch_filename(item.name)
+
+    print(item.file)
+
+    if fileName == vals.name and wd == vals.path then
+      item.icon = item.icon or Snacks.util.icon(item.ft, 'filetype')
+      item.branch = item.branch and ('branch:%s'):format(item.branch) or ''
+      scratchPad.name = item.name
+      scratchPad.icon = item.icon
+      scratchPad.file = item.file
+      scratchPad.ft = item.ft
+      break
+    end
+  end
+
+  if scratchPad ~= {} and scratchPad.name ~= nil then
+    --open_scratch(scratchPad, vals)
+    return scratchPad
+  else
+    return nil
+  end
+end
+
 local toggle_scratch_pad = function()
   local buffers = Snacks.scratch.list()
   local bufnr = vim.api.nvim_get_current_buf()
   local vals = buffer_vals(bufnr)
 
+  local pad = nil
   if #buffers > 0 then
-    reload_scratch_pad(buffers, vals)
+    pad = reload_scratch_pad(buffers, vals)
+  end
+
+  if pad ~= nil then
+    vals.fmtName = pad.name
+    vals.ft = pad.ft
+    vals.file = pad.file
+    vals.icon = pad.icon
+    open_scratch(vals)
   else
     open_new_scratch_pad(vals)
+    --open_scratch(vals)
   end
 end
 
