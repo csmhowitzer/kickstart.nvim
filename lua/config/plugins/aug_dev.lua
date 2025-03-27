16e627890c8af3b8516f3b95085453285a1bd624
csmhowitzer <csmhowitzer@gmail.com>
2025-03-27 15:47:40 -0400

    Refactoring chat to start getting it to work

---
+++ lua/config/plugins/aug_dev.lua
@@ -0,0 +1,12 @@
+return {
+  -- {
+  --   dir = '~/plugins/augment_chat.nvim',
+  --   dependencies = {
+  --     'folke/snacks.nvim',
+  --   },
+  --   name = 'augment_chat',
+  --   config = function()
+  --     require 'augment_chat'
+  --   end,
+  -- },
+}
